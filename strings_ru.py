a8b07014e304e2b29f01dd3929d9d5fd8827c53e
Alexey <57506563+Aleks-ir@users.noreply.github.com>
2020-10-08 18:33:10 +0300

    Version 2.0

---
+++ strings_ru.py
@@ -0,0 +1,63 @@
+start_game = 'Начать игру'
+options = 'Опции'
+about_game = 'Об игре'
+quit = 'Выход'
+server = 'Создать комнату'
+info_server = 'Новая комната'
+client = 'Войти в комнату'
+info_client = 'Присоедениться'
+back = 'Назад'
+accept = 'Принять'
+play_as = "Играть за"
+emperor = "Император"
+slave = "Раб"
+mode = "Режим"
+usual = "Обычный"
+with_change = "Со сменой"
+count_round = "Раундов"
+info_ip = "Введите ip-адрес комнаты:"
+info_last_ip = "Последний введенный ip:"
+about_cards = "Описание карт"
+rules_game = "Правила игры"
+control = "Управление"
+
+
+array_menu_strings = (start_game, options, about_game, quit, server, client, back, accept, play_as, count_round, mode,
+                      emperor, slave, usual, with_change, info_server, info_client, options, about_game, info_ip, info_last_ip,
+                      about_cards, rules_game, control)
+
+
+continue_game = "Продолжить"
+main_menu = "Главное меню"
+info_connection = "IP комнаты: "
+info_disconnection = "Ваш соперник отключился"
+victory = "Поздравляю. Вы победили :)"
+defeat = "Очень жаль, но вы проиграли :("
+
+array_dialogue_strings = (continue_game, main_menu, info_connection, info_disconnection, victory, defeat)
+
+
+array_rules_game = ("   Каждый игрок получет карту императора,",
+                    "либо раба и по 4 карты горожанина.",
+                    "Оба игрока скидывают на стол по одной ",
+                    "карте и подтверждают свой выбор.",
+                    "   За победу в раунде на стороне раба ",
+                    "дается 5 очков, на стороне императора - 1.",
+                    "Продолжительность игры зависит от количества",
+                    "раундов.")
+array_about_cards = ("•   Карта Императора бьет Горожанина, ",
+                     "но беззащитна против нападения Раба.",
+                     "•   Карта Раба беззащитна против карты",
+                     "Горожанина, но способна победить Императора.",
+                     "•   Карты Горожане равны по силе.",
+                     "Горожанин беззащитен против Императора.")
+array_control = ("ЛКМ - переместить карту",
+                      "ПКМ - подтвердить выбор",
+                      "СКМ - вернуть карты на место",
+                      "Esc - меню/назад")
+
+array_about_author = ("Автор и разработчик:",
+                      "Alexey Tisserand",
+                      "",
+                      "Тестировщик музыки:",
+                      "Max Rudko")
